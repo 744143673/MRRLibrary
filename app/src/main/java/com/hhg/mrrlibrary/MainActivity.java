6026ab43606a7c62f9be6af17ef136ace654e466
744143673 <hhg673@foxamil.com>
2017-10-05 11:39:15 +0800

    1.0

---
+++ app/src/main/java/com/hhg/mrrlibrary/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hhg.mrrlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
