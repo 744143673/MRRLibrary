7de9b76fcab6d8033f7d584f1d1b111eff03e266
744143673 <hhg673@foxamil.com>
2017-10-05 13:36:34 +0800

    comm gradle2

--- app/src/main/java/com/hhg/mrrlibrary/MainActivity.java
+++ app/src/main/java/com/hhg/mrrlibrary/MainActivity.java
@@ -2,6 +2,10 @@ package com.hhg.mrrlibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.hhg.mrrlibrary.utils.LogUtils;
+import com.hhg.mrrlibrary.utils.ToastUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LogUtils.i("MRRLibrary");
+        ToastUtils.showShort("MRRLibrary");
     }
 }
